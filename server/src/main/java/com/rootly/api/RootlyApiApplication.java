c2505810ad77268d56658da6ce6e2e25f0ea7273
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2026-09-12 15:17:35 -0300

    chore(server): remove redundant startup print

--- server/src/main/java/com/rootly/api/RootlyApiApplication.java
+++ server/src/main/java/com/rootly/api/RootlyApiApplication.java
@@ -8,6 +8,5 @@ public class RootlyApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RootlyApiApplication.class, args);
-        System.out.println("======= START API =======");
     }
 }
